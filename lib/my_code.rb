aeb381bd49264ccaa88bea8edb32a38b172b44d9
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-08-29 01:57:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -26,12 +26,7 @@ def map_to_double(source_array)
 end 
 
 def map_to_square(source_array)
-  count = 0
-  while count < source_array.length do
-    source_array[count] = source_array[count] ** 2
-    count += 1 
-  end
-  return source_array
+  source_array.map
 end 
 
 def reduce_to_total(source_array, starting_point = 0)
