7f32376ae9ee88bb173b2c27566dba9c3d0bd263
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-08-29 02:12:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,6 +21,6 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  return source_array.reduce() {|el| el.all}
+  return source_array.reduce().all? {|el| el == true}
 end
   
